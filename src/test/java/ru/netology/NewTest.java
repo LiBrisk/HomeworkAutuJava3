8a5cded0ba9e01b223f5712fd117e6adc34134ab
LiBrisk <kseniavolga3421@gmail.com>
2023-10-18 23:46:09 +0200

    Fixed Selenide code

--- src/test/java/ru/netology/NewTest.java
+++ src/test/java/ru/netology/NewTest.java
@@ -1,109 +1,94 @@
 package ru.netology;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class NewTest {
-    private WebDriver driver;
-    ChromeOptions options;
+    static {
+        Configuration.browser = "chrome";
 
-
-    @BeforeAll
-    static void SetUpAll() {
-
-        WebDriverManager.chromedriver().setup();
     }
+    static void setUpAll() {
 
-    @BeforeEach
-    void SetUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
+        getWebDriver().manage().window().maximize();
     }
-
-    @AfterEach
-    void tearDown() {
-        driver.quit();
-        driver = null;
-    }
-
     @Test
     void shouldBeSuccessfullResalt() {
-        driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов Иван");
-        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79398229211");
-        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
-        driver.findElement(By.tagName("button")).click();
-        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        String actual = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
-        Assertions.assertEquals(expected, actual);
+        open("http://localhost:9999");
+        $("[data-test-id='name'] input").setValue("Иванов Иван");
+        $("[data-test-id='phone'] input").setValue("+79398229211");
+        $("[data-test-id='agreement']").click();
+        $("button").click();
 
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = $("[data-test-id='order-success']").getText().trim();
+        assertEquals(expected, actual);
     }
 
     @Test
     void shouldBeBagWithNameWithNumber() {
-        driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов 1234");
-        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79398229211");
-        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
-        driver.findElement(By.tagName("button")).click();
+        open("http://localhost:9999");
+        $("[data-test-id='name'] input").setValue("Иванов 1234");
+        $("[data-test-id='phone'] input").setValue("+79398229211");
+        $("[data-test-id='agreement']").click();
+        $(By.tagName("button")).click();
         String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
-        String actual = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText().trim();
-        Assertions.assertEquals(expected, actual);
+        String actual = $("[data-test-id='name'].input_invalid .input__sub").getText().trim();
+        assertEquals(expected, actual);
     }
 
     @Test
     void shouldBeBagWithShortNumberPhone() {
-        driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов Иван");
-        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+7939822921");
-        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
-        driver.findElement(By.tagName("button")).click();
+        open("http://localhost:9999");
+        $("[data-test-id='name'] input").setValue("Иванов Иван");
+        $("[data-test-id='phone'] input").setValue("+7939822921");
+        $("[data-test-id='agreement']").click();
+        $(By.tagName("button")).click();
         String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
-        String actual = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub")).getText().trim();
-        Assertions.assertEquals(expected, actual);
+        String actual = $("[data-test-id='phone'].input_invalid .input__sub").getText().trim();
+        assertEquals(expected, actual);
     }
 
     @Test
     void shouldBeBagWithoutName() {
-        driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("");
-        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+7939822921");
-        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
-        driver.findElement(By.tagName("button")).click();
+        open("http://localhost:9999");
+        $("[data-test-id='name'] input").setValue("");
+        $("[data-test-id='phone'] input").setValue("+7939822921");
+        $("[data-test-id='agreement']").click();
+        $(By.tagName("button")).click();
         String expected = "Поле обязательно для заполнения";
-        String actual = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText().trim();
-        Assertions.assertEquals(expected, actual);
+        String actual = $("[data-test-id='name'].input_invalid .input__sub").getText().trim();
+        assertEquals(expected, actual);
     }
 
     @Test
     void shouldBeBagWithoutNumberPhone() {
-        driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов Иван");
-        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("");
-        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
-        driver.findElement(By.tagName("button")).click();
+        open("http://localhost:9999");
+        $("[data-test-id='name'] input").setValue("Иванов Иван");
+        $("[data-test-id='phone'] input").setValue("");
+        $("[data-test-id='agreement']").click();
+        $(By.tagName("button")).click();
         String expected = "Поле обязательно для заполнения";
-        String actual = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub")).getText().trim();
-        Assertions.assertEquals(expected, actual);
+        String actual = $("[data-test-id='phone'].input_invalid .input__sub").getText().trim();
+        assertEquals(expected, actual);
     }
 
     @Test
     void shouldBeBagWihtoutAgreement() {
-        driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов Иван");
-        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79398220921");
-        driver.findElement(By.tagName("button")).click();
+        open("http://localhost:9999");
+        $("[data-test-id='name'] input").setValue("Иванов Иван");
+        $("[data-test-id='phone'] input").setValue("+79398220921");
+        $(By.tagName("button")).click();
         String expected = "Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй";
-        String actual = driver.findElement(By.className("checkbox__text.input_invalid .input__sub")).getText().trim();
+        String actual = $("checkbox__text.input_invalid .input__sub").getText().trim();
 
-        Assertions.assertEquals(expected, actual);
+        assertEquals(expected, actual);
     }
 }
 
